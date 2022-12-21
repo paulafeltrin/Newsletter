dd6156c6739e5cc323fa52991db8e5af366c1088
Paula Cassia Feltrin <feltrin.paulacassia@gmail.com>
2022-12-21 15:40:45 -0300

    melhorias na validação do formulário

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-let inputEmail = document.getElementById("email")
-let botaoAviso = document.getElementById("aviso");
+const form = document.getElementById('form')
+const email = document.getElementById('email')
 
-botaoAviso.onclick = function () {
-    if(!inputEmail.value){
-        alert("Digite seu e-mail para receber a nossa newsletter")
-    }else{
-        alert(`Parabéns! \nVocê receberá as melhores notícias diratamente de seu e-mail! \nE-mail cadastrado: ${inputEmail.value}`)
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    checkInput()
+})
+
+function checkInput() {
+    const emailValue = email.value.trim()
+
+    if(emailValue === '') {
+        setErrorFor(email, 'Preencha esse campo')
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email inválido')
+    } else {
+        setSuccessFor(email)
+        alert("e-mail adicionado com sucesso!")
     }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+    small.innerText = message
+    formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success'
+}
+
+function isEmail(email) {
+    return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
 }
